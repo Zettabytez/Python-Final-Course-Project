e4d99a0478ee4186b7697213bf37a4f4ffaeabeb
zettabyte <zettabyte@mail.ru>
2022-09-16 18:13:39 +0700

    supplier_image_upload

--- changeImage.py
+++ changeImage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""from PIL import Image
+from PIL import Image
 from os import listdir
 import re
 
@@ -10,18 +10,4 @@ img_files = [ f for f in listdir(src) if ".tiff" in f ]
 
 for file in img_files:
     src_img = Image.open(src + file)
-    new_img = src_img.resize((600,400)).convert("RGB")
-    new_file = re.sub(".tiff", ".jpeg", file)
-    new_img.save(src + new_file, "JPEG")
-"""
-
-from PIL import Image
-import os
-
-path = "./supplier-data/images/"
-for f in os.listdir("./supplier-data/images"):
-    if f.endswith(".tiff"):
-        split_f = f.split(".")
-        name = split_f[0] + ".jpeg"
-        im = Image.open(path + f).convert("RGB")
-        im.resize((600, 400)).save("./supplier-data/images/" + name, "JPEG")
+    new_img = src_img.resize((600,400)).convert("RGB").save(src + re.sub(".tiff", ".jpeg", file), "JPEG")
---
+++ supplier_image_upload.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import requests
+import os
+
+url = "http://localhost/upload/"
+for f in os.listdir("./supplier-data/images"):
+    if ".jpeg" in f:
+        with open('./supplier-data/images/' + f, 'rb') as opened:
+            r = requests.post(url, files={'file': opened})
